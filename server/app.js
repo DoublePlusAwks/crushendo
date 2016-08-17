249849b44c8b51633798bb2ced780116b3650a9b
Spencer Kim <sabin1997@gmail.com>
2016-08-17 01:04:27 -0400

    adds endpoints

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ var SpotifyHelper = require('./spotify-helper');
 var spotify = new SpotifyHelper();
 var app = express();
 
-app.use(bodyParser.text());
+app.use(bodyParser.json());
 
 app.get('/monitor', function(request, response)  {
   response.send('Status: OK');
@@ -22,10 +22,30 @@ app.get('/test', function(request, response)  {
 
 app.post('/autocomplete', function(request, response) {
   var start = Date.now();
-  spotify.search(request.body).then(function(result)  {
+  spotify.search(request.body.query).then(function(result)  {
     console.log(Date.now() - start);
     response.json(result);
   });
+});
+
+app.post('/recommendations', function(request, response)  {
+  var start = Date.now();
+  spotify.getRecommendations(request.body.artists, request.body.tracks, 10, 145, 0.8, 0.5)
+    .then(function(result)  {
+      console.log(Date.now() - start);
+      response.json(result);
+    }, function(err)  {
+      console.log(err);
+    });
+});
+
+app.post('/trackinfo', function(request, response)  {
+  spotify.getAudioFeatures(request.body.query)
+    .then(function(result)  {
+      response.json(result);
+    }, function(err)  {
+      console.log(err);
+    });
 });
 
 app.listen(PORT, function() {
