432d5c33dffe3825061a4d8b9d93eef5a3d45a45
Spencer Kim <sabin1997@gmail.com>
2016-08-19 15:17:25 -0400

    adds list for output

--- public/js/form.js
+++ public/js/form.js
@@ -16,12 +16,11 @@ $(function()  {
       search = search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
       var re = new RegExp("(" + search.split(' ').join('|') + ")", "gi");
       console.log(item.name);
-      return '<div class="autocomplete-suggestion" data-id="' + item.id + '" data-uri="' + item.uri + '" data-name= "' + item.name + '">' + '<img src="' + item.image + '"/>' + item.name.replace(re, "<b>$1</b>") + '</div>';
+      return '<div class="autocomplete-suggestion" data-id= "' + item.id + '" data-name="' + item.name + '">' + '<img src="' + item.image + '"/>' + item.name.replace(re, "<b>$1</b>") + '</div>';
     },
     cache: true,
     onSelect: function(e, term, item) {
-      alert('Item "'+item.data('name')+' ('+item.data('id')+')" selected by '+(e.type == 'keydown' ? 'pressing enter' : 'mouse click')+'.');
-      $('#seeds').append(item.data('uri'));
+      $('#seeds').append('<li class="list-group-item">' + item.data('name') + '</li>');
     }
   };
 
